9c9f2d1b5727aa3467bac42be511779c0e7607ce
mdmehedyhassan <mdmehedyh55@gmail.com>
2021-03-20 21:53:52 +0600

    search

--- src/components/Destination/Destination.js
+++ src/components/Destination/Destination.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useParams } from 'react-router';
+import fakeData from '../../fakeData/fakeData.json'
 
 const Destination = () => {
+    const [ridersBook, setRidersBook] = useState(true);
+    let { name } = useParams();
+    const rider = fakeData.find(rides => rides.name === name)
+
     return (
         <div>
             <div className="row">
                 <div className="col-md-4">
-                    <div className="bg-light p-5">
+                    {ridersBook ? <div className="bg-light p-5">
                         <label htmlFor="">Pick From</label>
                         <input className="w-100 mb-4" type="text" placeholder="Mirpor 1" />
                         <label htmlFor="">Pick to</label>
@@ -13,9 +19,29 @@ const Destination = () => {
                         <label htmlFor="">Pick time</label>
                         <input className="w-100 mb-4" type="time" name="" id="" />
                         <br />
-                        <button className="btn btn-warning w-100">Search</button>
-                    </div>
-                    
+                        <button onClick={() => setRidersBook(!ridersBook)} className="btn btn-warning w-100">Search</button>
+                    </div> : <div className="d-flex flex-column bd-highlight mb-3 bg-light p-5">
+                        <div className="p-4 mb-2 justify-content-between w-100 d-flex flex-column bg-warning text-light  border border-info bd-highlight rounded-pill">
+                            <h4>Mirpor 1</h4>
+                            <h4>Danmondi</h4>
+                        </div>
+                        <div className="p-2 mb-2 justify-content-between w-100 d-flex flex-row  border border-info bd-highlight rounded-pill">
+                            <img style={{ width: '30px' }} src={rider.image} alt="" />
+                            <p>{rider.name}</p>
+                            <p>${rider.price}</p>
+                        </div>
+                        <div className="p-2 mb-2 justify-content-between w-100 d-flex flex-row  border border-info bd-highlight rounded-pill">
+                            <img style={{ width: '30px' }} src={rider.image} alt="" />
+                            <p>{rider.name}</p>
+                            <p>${rider.price}</p>
+                        </div>
+                        <div className="p-2 mb-2 justify-content-between w-100 d-flex flex-row  border border-info bd-highlight rounded-pill">
+                            <img style={{ width: '30px' }} src={rider.image} alt="" />
+                            <p>{rider.name}</p>
+                            <p>${rider.price}</p>
+                        </div>
+                    </div>}
+
                 </div>
                 <div className="col-md-8">
                     <img src="https://i.ibb.co/zh3MC60/Map.png" alt="" />
